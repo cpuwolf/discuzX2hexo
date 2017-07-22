b65d631227a934f9b15e978132fa1351e61af2de
Wei Shuai <cpuwolf@gmail.com>
2017-07-23 05:11:57 +0800

    optimize .py

--- discuzx2md.py
+++ discuzx2md.py
@@ -23,26 +23,46 @@ def urltolink(line):
 	#convert url to link
 	#[url=A]B[/url]   =>   [B](A)
 	#[url]A[/url]  => [](A)
-	line=re.sub(r'\[url\=(.[^\[]*)\](.*)\[\/url\]',r'\r\n\r\n[\2](\1)\r\n\r\n',line)
-	line=re.sub(r'\[url\](.*)\[\/url\]',r'\r\n\r\n[\1](\1)\r\n\r\n',line)
+	line=re.sub(r'\[url\=(.[^\[]*)\](.*)\[\/url\]',r'[\2](\1)',line)
+	line=re.sub(r'\[url\](.*)\[\/url\]',r'[\1](\1)',line)
 	#[img]
-	line=re.sub(r'\[img\=(.[^\[]*)\](.*)\[\/img\]',r'\r\n\r\n![cpuwolfx-plane](\2)\r\n\r\n',line)
-	line=re.sub(r'\[img\](.*)\[\/img\]',r'\r\n\r\n![cpuwolfx-plane](\1)\r\n\r\n',line)
+	line=re.sub(r'\[img\=(.[^\[]*)\](.*)\[\/img\]',r'\r\n\r\n![](\2)\r\n\r\n',line)
+	line=re.sub(r'\[img\](.*)\[\/img\]',r'\r\n\r\n![](\1)\r\n\r\n',line)
 	#[size]
-	line=re.sub(r'\[size\=(.[^\[]*)\](.*)\[\/size\]',r'\r\n\2\r\n',line)
-	line=re.sub(r'\[size\](.*)\[\/size\]',r'\r\n\1\r\n\r',line)
+	line=re.sub(r'\[size\=(.[^\[]*)\](.*)\[\/size\]',r'\2',line)
+	line=re.sub(r'\[size\](.*)\[\/size\]',r'\1',line)
+	#[yun]A[/yun]  => [](A)
+	line=re.sub(r'\[yun\=(.[^\[]*)\](.*)\[\/yun\]',r'[password:\2](\1)',line)
+	line=re.sub(r'\[yun\](.*)\[\/yun\]',r'[\1](\1)',line)
+	#[backcolor]
+	line=re.sub(r'\[backcolor\=(.[^\[]*)\](.*)\[\/backcolor\]',r'\2',line)
+	line=re.sub(r'\[backcolor\](.*)\[\/backcolor\]',r'\1',line)
+	#[color]
+	line=re.sub(r'\[color\=(.[^\[]*)\](.*)\[\/color\]',r'\2',line)
+	line=re.sub(r'\[color\](.*)\[\/color\]',r'\1',line)
+	#[font]
+	line=re.sub(r'\[font\=(.[^\[]*)\](.*)\[\/font\]',r'\2',line)
+	line=re.sub(r'\[font\](.*)\[\/font\]',r'\1',line)
+	#[b]
+	line=re.sub(r'\[b\](.*)\[\/b\]',r'**\1**',line)
 	#[i=s]
 	line=re.sub(r'\[i\=s\](.*)\[\/i\]',r'\r\n\r',line)
+
 	
 	#remove like [/color]
 	line=re.sub(r'\[\/(.[^\[]*)\]',r'\r\n',line)
 	#remove like [color=xx]
-	line=re.sub(r'\[size(.[^\[]*)\]',r'\r\n',line)
-	line=re.sub(r'\[font(.[^\[]*)\]',r'\r\n',line)
-	line=re.sub(r'\[color(.[^\[]*)\]',r'\r\n',line)
-	line=re.sub(r'\[align(.[^\[]*)\]',r'\r\n',line)
-	line=re.sub(r'\[media(.[^\[]*)\]',r'\r\n',line)
+	line=re.sub(r'\[size(.[^\[]*)\]',r'',line)
+	line=re.sub(r'\[font(.[^\[]*)\]',r'',line)
+	line=re.sub(r'\[color(.[^\[]*)\]',r'',line)
+	line=re.sub(r'\[align(.[^\[]*)\]',r'',line)
+	line=re.sub(r'\[media(.[^\[]*)\]',r'',line)
+	line=re.sub(r'\[list(.[^\[]*)\]',r'',line)
+	line=re.sub(r'\[p(.[^\[]*)\]',r'',line)
 	line=re.sub(r'\[hide\]',r'\r\n',line)
+
+	line=re.sub(r'\[list\]',r'\r\n',line)
+	line=re.sub(r'^\[\*\]',r'* ',line)
 	return line
 
 fout=open("out/blog.txt", "w")
@@ -86,6 +106,6 @@ for stext in wholetextsinglefile:
 			smd.write('## '+filesec[1].strip()+'\r\n\r\n')
 			smd.write(filesec[2])
 			smd.close()
-			os.system('iconv -t utf-8 -f gb2312 ' + 'out/vip_old'+str(idx)+'.md' + '> blog/source/_posts/vip_old'+str(idx)+'.md')
+			os.system('iconv -c -t utf-8 -f gb2312 ' + 'out/vip_old'+str(idx)+'.md' + '> blog/source/_posts/vip_old'+str(idx)+'.md')
 		idx=idx+1
 fpre.close()
