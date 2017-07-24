25a4705a5b6ee6a226bb0d27b72feca11d5fdfd6
Wei Shuai <cpuwolf@gmail.com>
2017-07-24 14:43:56 +0800

    add useful comments

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # discuzX2hexo
+
+DiscuzX的论坛关闭了，那么数据库MySQL的帖子要是直接丢弃那就太可惜了。所以我花了一天时间，研究了一下怎么不让这些历史数据浪费。
+
+首先进入mysql，使用数据库ultrax
+```
+use ultrax;
+```
+字符集的问题
+
+```
+set names utf8;
+```
+备份一下所有人的联络方式，导出为.csv数据表文件
+
+```
+select email,username,password from pre_common_member into outfile '/tmp/mycontact.csv'  fields terminated by ','   optionally enclosed by '"'   lines terminated by '\n'; 
+```
+这个是重点，导出所有的楼主贴子内容，用';;;'隔离字段，用'|||\r\n'隔离行，以备后续处理
+
+```
+select from_unixtime(dateline),subject,message from pre_forum_post where first=1 into outfile '/tmp/mytitle6.csv'  fields terminated by ';;;'   lines terminated by '|||\r\n';
+```
+最头痛的是帖子里的图片[attach]你需要知道他们所以在路径，而这个路径就放在数据库表格
+pre_forum_attachment
+
+```
+select aid,tableid from pre_forum_attachment limit 1,6;
+```
+
+可惜这个表格只是个所有查询，真正的图片路径在分表格pre_forum_attachment_xx
+分表格你可以随便看看
+
+```
+select aid,tid,attachment from pre_forum_attachment_9 limit 1,10;
+```
+下面这段联表查询，我mySQL不太熟，只能用笨办法，导出为csv
+
+```
+select * from ((select aid,attachment from pre_forum_attachment_0) union (select aid,attachment from pre_forum_attachment_1) union (select aid,attachment from pre_forum_attachment_2) union (select aid,attachment from pre_forum_attachment_3) union (select aid,attachment from pre_forum_attachment_4) union (select aid,attachment from pre_forum_attachment_5) union (select aid,attachment from pre_forum_attachment_6) union (select aid,attachment from pre_forum_attachment_7) union (select aid,attachment from pre_forum_attachment_8) union (select aid,attachment from pre_forum_attachment_9)) A order by aid into outfile '/tmp/myattachlist.csv'  fields terminated by ','   optionally enclosed by '"'   lines terminated by '\n';
+```
+
+Discuz的一些有用的菜单上的链接
+
+```
+select url from pre_common_nav;
+```
+
+友情链接
+
+```
+select * from pre_common_friendlink;
+```
+
+现在我拿到了一堆的csv文件，下一步我准备把他们导出为一个个markdown文件，作为博客发布
--- discuzx2md.py
+++ discuzx2md.py
@@ -1,90 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on July 2017
+
+@author: Wei Shuai <cpuwolf@gmail.com>
+"""
+
 import re
 import codecs
 import os
 
 attachlist = []
 
+# get attachment list
 fatch=open("myattachlist.csv")
 
 for line in fatch.readlines():
-	attachlist.append(line.strip().split(','))
+    attachlist.append(line.strip().split(','))
 
 fatch.close()
 
 print attachlist
 
 def id2src(id):
-	for a in attachlist:
-		if a[0] == id:
-			return a[1]
-	return ''
+    for a in attachlist:
+        if a[0] == id:
+            return a[1]
+    return ''
 
-def urltolink(line):
-	#convert url to link
-	#[url=A]B[/url]   =>   [B](A)
-	#[url]A[/url]  => [](A)
-	line=re.sub(r'\[url\=(.[^\[]*)\](.*)\[\/url\]',r'[\2](\1)',line)
-	line=re.sub(r'\[url\](.*)\[\/url\]',r'[\1](\1)',line)
-	#[img]
-	line=re.sub(r'\[img\=(.[^\[]*)\](.*)\[\/img\]',r'\r\n\r\n![](\2)\r\n\r\n',line)
-	line=re.sub(r'\[img\](.*)\[\/img\]',r'\r\n\r\n![](\1)\r\n\r\n',line)
-	#[size]
-	line=re.sub(r'\[size\=(.[^\[]*)\](.*)\[\/size\]',r'\2',line)
-	line=re.sub(r'\[size\](.*)\[\/size\]',r'\1',line)
-	#[yun]A[/yun]  => [](A)
-	line=re.sub(r'\[yun\=(.[^\[]*)\](.*)\[\/yun\]',r'[password:\2](\1)',line)
-	line=re.sub(r'\[yun\](.*)\[\/yun\]',r'[\1](\1)',line)
-	#[backcolor]
-	line=re.sub(r'\[backcolor\=(.[^\[]*)\](.*)\[\/backcolor\]',r'\2',line)
-	line=re.sub(r'\[backcolor\](.*)\[\/backcolor\]',r'\1',line)
-	#[color]
-	line=re.sub(r'\[color\=(.[^\[]*)\](.*)\[\/color\]',r'\2',line)
-	line=re.sub(r'\[color\](.*)\[\/color\]',r'\1',line)
-	#[font]
-	line=re.sub(r'\[font\=(.[^\[]*)\](.*)\[\/font\]',r'\2',line)
-	line=re.sub(r'\[font\](.*)\[\/font\]',r'\1',line)
-	#[b]
-	line=re.sub(r'\[b\](.*)\[\/b\]',r'**\1**',line)
-	#[i=s]
-	line=re.sub(r'\[i\=s\](.*)\[\/i\]',r'\r\n\r',line)
+# preprocess marks
+def preproc(line):
+    #convert url to link
+    #[url=A]B[/url]   =>   [B](A)
+    #[url]A[/url]  => [](A)
+    line=re.sub(r'\[url\=(.[^\[]*)\](.*)\[\/url\]',r'[\2](\1)',line)
+    line=re.sub(r'\[url\](.*)\[\/url\]',r'[\1](\1)',line)
+    #[media=A]B[/media]  => [B](A)
+    line=re.sub(r'\[media\=(.[^\[]*)\](.*)\[\/media\]',r'<video>\2</video>',line)
+    line=re.sub(r'\[media\](.*)\[\/media\]',r'<video>\1</video>',line)
+    #[flash=A]B[/flash]  => [B](A)
+    line=re.sub(r'\[flash\=(.[^\[]*)\](.*)\[\/flash\]',r'<embed src="\2" height="320" width="240"/>',line)
+    line=re.sub(r'\[flash\](.*)\[\/flash\]',r'<embed src="\1" height="320" width="240"/>',line)
+    #[img]
+    line=re.sub(r'\[img\=(.[^\[]*)\](.*)\[\/img\]',r'\r\n\r\n![](\2)\r\n\r\n',line)
+    line=re.sub(r'\[img\](.*)\[\/img\]',r'\r\n\r\n![](\1)\r\n\r\n',line)
+     #[p]
+    line=re.sub(r'\[p\=(.[^\[]*)\](.*)\[\/p\]',r'\2',line)
+    line=re.sub(r'\[p\](.*)\[\/p\]',r'\1',line)
+    #[size]
+    line=re.sub(r'\[size\=(.[^\[]*)\](.*)\[\/size\]',r'\2',line)
+    line=re.sub(r'\[size\](.*)\[\/size\]',r'\1',line)
+    #[yun]A[/yun]  => [](A)
+    line=re.sub(r'\[yun\=(.[^\[]*)\](.*)\[\/yun\]',r'[password:\2](\1)',line)
+    line=re.sub(r'\[yun\](.*)\[\/yun\]',r'[\1](\1)',line)
+    #[font]
+    line=re.sub(r'\[font\=(.[^\[]*)\](.*)\[\/font\]',r'\2',line)
+    line=re.sub(r'\[font\](.*)\[\/font\]',r'\1',line)
+    #[b]
+    line=re.sub(r'\[b\](.*)\[\/b\]',r'**\1**',line)
+    #[i=s]
+    line=re.sub(r'\[i\=s\](.*)\[\/i\]',r'\r\n\r',line)
+    #[backcolor]
+    line=re.sub(r'\[backcolor\=(.[^\[]*)\](.*)\[\/backcolor\]',r'\2',line)
+    line=re.sub(r'\[backcolor\](.*)\[\/backcolor\]',r'\1',line)
+    #[color]
+    line=re.sub(r'\[color\=(.[^\[]*)\](.*)\[\/color\]',r'\2',line)
+    line=re.sub(r'\[color\](.*)\[\/color\]',r'\1',line)
+     #[align]
+    line=re.sub(r'\[align\=(.[^\[]*)\](.*)\[\/align\]',r'\2',line)
+    line=re.sub(r'\[align\](.*)\[\/align\]',r'\1',line)
+     #[list]
+    line=re.sub(r'\[list\=(.[^\[]*)\](.*)\[\/list\]',r'\2',line)
+    line=re.sub(r'\[list\](.*)\[\/list\]',r'\1',line)
+     #[hide]
+    line=re.sub(r'\[hide\=(.[^\[]*)\](.*)\[\/hide\]',r'\2',line)
+    line=re.sub(r'\[hide\](.*)\[\/hide\]',r'\1',line)
 
-	
-	#remove like [/color]
-	line=re.sub(r'\[\/(.[^\[]*)\]',r'\r\n',line)
-	#remove like [color=xx]
-	line=re.sub(r'\[size(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[font(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[color(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[align(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[media(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[list(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[p(.[^\[]*)\]',r'',line)
-	line=re.sub(r'\[hide\]',r'\r\n',line)
 
-	line=re.sub(r'\[list\]',r'\r\n',line)
-	line=re.sub(r'^\[\*\]',r'* ',line)
-	return line
+    #remove all other alone no parm headers like [header=xx]
+    line=re.sub(r'\[font\=(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[color\=(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[size\=(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[align\=(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[list\=(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[backcolor\=(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[p(.[^\[]*)\]',r'',line)
+    line=re.sub(r'\[b\]',r'',line)
+    line=re.sub(r'\[hide\]',r'',line)
+    line=re.sub(r'\[list\]',r'',line)
+    line=re.sub(r'^\[\*\]',r'* ',line)
+     #remove all other alone footers like [/footer]
+    line=re.sub(r'\[\/(.[^\[]*)\]',r'\r\n',line)
+    return line
 
-fout=open("out/blog.txt", "w")
 
+# input file
 allmd=open("mytitle6.csv")
-
+# output file
+fout=open("out/preblog.txt", "w")
 
 for line in allmd.readlines():
-	#convert attach to markdown image
-	nl = re.findall('\[attach\](\d+)\[\/attach\]', line)
-	if nl:
-		for a in nl:
-			linkpth='/images/data/attachment/' + id2src(a).replace('"','')
-			line=re.sub(r'\[attach\]'+a+'\[\/attach\]','\r\n![cpuwolf]('+ linkpth +')\r\n',line)
-			print a
-	line=urltolink(line)
-	fout.write(line)
+    #convert attach to markdown image
+    nl = re.findall('\[attach\](\d+)\[\/attach\]', line)
+    if nl:
+        for a in nl:
+            linkpth='/images/data/attachment/' + id2src(a).replace('"','')
+            line=re.sub(r'\[attach\]'+a+'\[\/attach\]','\r\n![cpuwolf]('+ linkpth +')\r\n',line)
+            print a
+    line=preproc(line)
+    fout.write(line)
 
 allmd.close()
 fout.close()
+
+
 #preprocess is done, let's open it again
-fpre=open("out/blog.txt")
+
+
+# input file
+fpre=open("out/preblog.txt")
 
 wholetext=fpre.read()
 
@@ -92,19 +125,19 @@ wholetextsinglefile=wholetext.split('|||')
 
 idx=1
 for stext in wholetextsinglefile:
-	try:
-		filesec=stext.split(';;;')
-	except ValueError:
-		print "split done"
-	else:
-		if len(filesec) > 1 and len(filesec[1].strip()) > 1 and len(filesec[0].strip()) > 1:
-			smd=open('out/vip_old'+str(idx)+'.md','w')
-			smd.write('---\r\n')
-			smd.write(('title: '+filesec[1].strip()+'\r\n'))
-			smd.write('date: '+filesec[0].strip()+'\r\n')
-			smd.write('---\r\n\r\n')
-			smd.write(filesec[2])
-			smd.close()
-			os.system('iconv -c -t utf-8 -f gb2312 ' + 'out/vip_old'+str(idx)+'.md' + '> blog/source/_posts/forum_old'+str(idx)+'.md')
-		idx=idx+1
+    try:
+        filesec=stext.split(';;;')
+    except ValueError:
+        print "split done"
+    else:
+        if len(filesec) > 1 and len(filesec[1].strip()) > 1 and len(filesec[0].strip()) > 1:
+            smd=open('out/vip_old'+str(idx)+'.md','w')
+            smd.write('---\r\n')
+            smd.write(('title: '+filesec[1].strip()+'\r\n'))
+            smd.write('date: '+filesec[0].strip()+'\r\n')
+            smd.write('---\r\n\r\n')
+            smd.write(filesec[2])
+            smd.close()
+            os.system('iconv -c -t utf-8 -f gb2312 ' + 'out/vip_old'+str(idx)+'.md' + '> blog/source/_posts/forum_old'+str(idx)+'.md')
+        idx=idx+1
 fpre.close()
